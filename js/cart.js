7f769b3ff6689a9ce9fb3dac2ab13f684fc146b4
董聿凌 <gb2013429@gmail.com>
2023-03-01 19:56:27 +0800

    getProducts

---
+++ js/cart.js
@@ -0,0 +1,29 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js";
+
+const apiUrl = "https://vue3-course-api.hexschool.io/v2";
+const apiPath = "yuling202202";
+
+const app = createApp({
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    getProducts() {
+      axios
+        .get(`${apiUrl}/api/${apiPath}/products/all`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err.data.message);
+        });
+    },
+  },
+  mounted() {
+    this.getProducts();
+  },
+});
+
+app.mount("#app");
